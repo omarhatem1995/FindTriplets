85018962055571f22f723e05b6dcb7d92cd19bab
Omar Hatem <omar.kortam@cic.ae>
2024-03-31 21:40:51 +0200

    -Added Main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //array of triplets
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        //the sum to check
+        int targetSum = 0;
+        List<List<Integer>> result = FindTriplets.findTriplets(nums, targetSum);
+        //for loop to get the output of the triplets.
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
     }
 }
